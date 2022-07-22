6314c183c9fa29fda50de3f7e91f2bc42dfda399
syed muhammad bilal <102545337+syedmuhammadbilalshakeel@users.noreply.github.com>
2022-07-22 22:14:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// function tim(){
+
+// var back=["red" ,"blue" ];
+// var randomnu= back[math.floor(math.random)*1]
+
+// var cha = document.getElementById("back")
+// cha.style.background=background[math.floor]
+
+// // var change =document. []
+
+
+// }
+// tim();
+
+
+var bg = document.getElementById("background");
+function backgroundColor() {
+    var colors = ["red", "blue", "rgb(102, 255, 102)", "rgb(0, 204, 255)", "rgb(255, 0, 255)"];
+    bg.style.background = colors[Math.round(Math.random() * 2)];
+    setTimeout(backgroundColor, 1000)
+}
+backgroundColor();
+
+
+
+// var random()
+// {
+
+// }
+
+
